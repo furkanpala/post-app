6fde14e268020a7594a7a96ccf6829830fb732ec
furkanpala <furkan2pala@gmail.com>
2020-04-02 23:04:28 +0300

    Footer completed

--- web/post-app/src/components/Footer.vue
+++ web/post-app/src/components/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <footer class="footer">Footer</footer>
+  <footer class="footer">
+    <span class="me">Furkan Pala</span>
+    <span class="pipe">|</span>
+    <span class="github-link">
+      <a href="https://github.com/furkanpala">Github</a>
+    </span>
+  </footer>
 </template>
 
 <script>
@@ -10,7 +16,14 @@ export default {
 
 <style scoped>
 .footer {
-  background-color: green;
   flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.5);
+  min-height: 2rem;
+}
+
+.pipe {
+  margin: 0 0.5rem;
 }
 </style>
